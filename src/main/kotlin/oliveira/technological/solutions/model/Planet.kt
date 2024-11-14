f0c6aa6ba9621b69d4b86a89be9d3ea2cb8b5602
douglasor <douglasor@outlook.com>
2024-11-14 15:18:52 -0300

    add validation on Planet model

--- src/main/kotlin/oliveira/technological/solutions/model/Planet.kt
+++ src/main/kotlin/oliveira/technological/solutions/model/Planet.kt
@@ -2,13 +2,25 @@ package oliveira.technological.solutions.model
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+
 
 @Entity
 data class Planet(
     @Id
     val id: String? = null,
+
+    @field:NotEmpty
+    @field:Size(min = 3, max = 150)
     val name: String,
+
+    @field:NotEmpty
+    @field:Size(max = 150)
     val climate: String,
+
+    @field:NotEmpty
+    @field:Size(max = 250)
     val terrain: String
 )
 
